60695af46b991f7dfca3ef0cb45d318d68e930ad
yangchen <ada_8463@163.com>
2015-04-16 23:58:36 +0800

    basically completed all tasks

--- src/java/servlet/TravelInfo.java
+++ src/java/servlet/TravelInfo.java
@@ -17,17 +17,27 @@ import controller.GetScenery;
 import controller.MarshalTravelInfo;
 import controller.UnmarshalRestaurant;
 import controller.UnmarshalWeather;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
 import java.net.URLDecoder;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
 
 /**
  *
@@ -47,44 +57,54 @@ public class TravelInfo extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        GetResource gr = new GetResource();
-        String r = request.getParameter("sid");
-        String[] sr = r.split("_");
-        String sid = sr[0];
-        String title = URLDecoder.decode(sr[1], "gb2312");
-        String address = URLDecoder.decode(sr[2], "gb2312");
-        System.out.println(title);
-        System.out.println(address);
-        HttpSession session = request.getSession();
-        String cityName = (String) session.getAttribute("cityName");
-        System.out.println(cityName);
+            GetResource gr = new GetResource();
+            String r = request.getParameter("sid");
+            String[] sr = r.split("_");
+            String sid = sr[0];
+            String title = URLDecoder.decode(sr[1], "gb2312");
+            String address = URLDecoder.decode(sr[2], "gb2312");
+            System.out.println(title);
+            System.out.println(address);
+            HttpSession session = request.getSession();
+            String cityName = (String) session.getAttribute("cityName");
+            System.out.println(cityName);
 //        request.setAttribute("sid", sid);
 //        request.getRequestDispatcher("output.jsp").forward(request, response);
-        GetScenery gs = new GetScenery();
-        GetGeocoding gg = new GetGeocoding();
-        UnmarshalRestaurant unrt = new UnmarshalRestaurant();
-        UnmarshalWeather unw = new UnmarshalWeather();
-        GetHotel gh = new GetHotel();
-        double[] geocoding = gg.getGeocoding(address);
-        String location = "" + geocoding[1] + "," + geocoding[0];
-        List<Scenery> listScenery = gs.getScenery(sid);
-        List<Result> restaurant = unrt.getData(location, "饭店");
-        List<Result> bank = unrt.getData(location, "银行");
-        Weather weather = unw.getWeather(cityName);
-        List<Hotel> listHotel =  gh.getHotel(cityName, geocoding);
-        Travel travelInfo = new Travel();
-        travelInfo.setTravelCity(cityName);
-        travelInfo.setTravelTitle(title);
-        travelInfo.setListBank(bank);
-        travelInfo.setListHotel(listHotel);
-        travelInfo.setListRestaurant(restaurant);
-        travelInfo.setWeather(weather);
-        travelInfo.setListScenery(listScenery);
-        
-//        PrintWriter out = response.getWriter();
-//        TransformerFactory fac = TransformerFactory.newInstance();
-        MarshalTravelInfo mti = new MarshalTravelInfo();
-        mti.getXml(travelInfo, getServletContext().getRealPath("/"));
+            GetScenery gs = new GetScenery();
+            GetGeocoding gg = new GetGeocoding();
+            UnmarshalRestaurant unrt = new UnmarshalRestaurant();
+            UnmarshalWeather unw = new UnmarshalWeather();
+            GetHotel gh = new GetHotel();
+            double[] geocoding = gg.getGeocoding(address);
+            String location = "" + geocoding[1] + "," + geocoding[0];
+            List<Scenery> listScenery = gs.getScenery(sid);
+            List<Result> restaurant = unrt.getData(location, "饭店");
+            List<Result> bank = unrt.getData(location, "银行");
+            Weather weather = unw.getWeather(cityName);
+            List<Hotel> listHotel =  gh.getHotel(cityName, geocoding);
+            Travel travelInfo = new Travel();
+            travelInfo.setTravelCity(cityName);
+            travelInfo.setTravelTitle(title);
+            travelInfo.setListBank(bank);
+            travelInfo.setListHotel(listHotel);
+            travelInfo.setListRestaurant(restaurant);
+            travelInfo.setWeather(weather);
+            travelInfo.setListScenery(listScenery);
+        try {     
+            PrintWriter out = response.getWriter();
+            TransformerFactory fac = TransformerFactory.newInstance();
+            Source xslt = new StreamSource(new File(getServletContext().getRealPath("/") + "WEB-INF/classes/xsl/travel.xsl"));
+            Transformer transformer = fac.newTransformer(xslt);
+            MarshalTravelInfo mti = new MarshalTravelInfo();
+            mti.getXml(travelInfo, getServletContext().getRealPath("/"));
+            URL url = new URL("http://localhost:8080/ITravel/travel.xml");
+            Source xml = new StreamSource(url.openStream());
+            transformer.transform(xml, new StreamResult(out));    
+        } catch (TransformerConfigurationException ex) {
+            Logger.getLogger(TravelInfo.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransformerException ex) {
+            Logger.getLogger(TravelInfo.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
